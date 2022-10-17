ed3ded0fb6749d25d54ba133bf22a814ed7fbc64
alex <kasperskiy.94@gmail.com>
2022-10-17 12:09:27 +0300

    delete slider day

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,8 @@
-$(".center").slick({
-    slidesToShow: 1,
-    speed: 500,
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="carousel-prev"><img src="icons/right.svg"></button>',
+        nextArrow: '<button type="button" class="carousel-next"><img src="icons/right.svg"></button>',
+    });
 });
